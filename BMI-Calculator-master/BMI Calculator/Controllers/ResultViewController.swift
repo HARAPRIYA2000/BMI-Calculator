9f1d4bc9ccf3c827ddb39588c5ba5fde4adff6af
HARAPRIYA JENA <131438513+HARAPRIYA2000@users.noreply.github.com>
2023-06-10 13:11:03 +0530

    Add files via upload

---
+++ BMI-Calculator-master/BMI Calculator/Controllers/ResultViewController.swift
@@ -0,0 +1,28 @@
+//
+// ResultViewController.swift
+// BMI Calculator
+//
+//Created by Harapriya on 09/11/2022
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+    var bmiValue: String?
+    var color: UIColor?
+    var advice: String?
+    @IBOutlet weak var adviceLabel: UILabel!
+    @IBOutlet weak var bmiLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        bmiLabel.text = bmiValue
+        adviceLabel.text = advice
+        view.backgroundColor = color
+    }
+    @IBAction func recaluclatePressed(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
+    
+}
---
+++ BMI-Calculator-master/BMI Calculator/Models/BMI.swift
@@ -0,0 +1,13 @@
+//
+// BMI.swift
+// BMI Calculator
+//
+//Created by Harapriya on 09/11/2022
+//
+
+import UIKit
+struct BMI{
+    let value: Float
+    let advice: String
+    let color: UIColor
+}
